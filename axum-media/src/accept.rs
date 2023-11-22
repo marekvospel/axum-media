a06478dd68b16e88de7ad8fab91876cdaa8432c5
Marek Vospěl <marek@vospel.cz>
2023-11-22 21:59:08 +0100

    docs: add Accept extractor docs

--- axum-media/src/accept.rs
+++ axum-media/src/accept.rs
@@ -2,6 +2,35 @@ use std::ops::{Deref, DerefMut};
 
 use axum::{extract::FromRequestParts, http::request::Parts};
 
+/// Accept header extractor.
+///
+/// Gets `Option<String>` from the request's `Accept` header, so it can be later used with [`crate::AnyMedia`].
+/// Thanks to [`axum::extract::FromRequestParts`] it can be used with other extractors including [`axum::Json`] or [`crate::AnyMedia`].
+///
+/// The request should never be rejected, as if the header is not present `None` will be used as the internal value.
+///
+/// ## Example
+/// ```rust,no_run
+/// use axum_media::Accept;
+///
+/// async fn handler(accept: Accept) -> impl axum::response::IntoResponse {
+///   // Can directly be used as Option<String>
+///   println!("Request accepts {}", accept.as_deref().unwrap());
+///
+///   axum_media::AnyMedia(
+///     serde_json::json!({}),
+///     accept.into(),
+///   )
+/// }
+///
+/// #[tokio::main]
+/// async fn main() {
+///   let app = axum::Router::new().route("/", axum::routing::get(handler));
+///
+///   axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+///     .serve(app.into_make_service()).await.unwrap();
+/// }
+/// ```
 #[derive(Debug, Clone)]
 pub struct Accept(Option<String>);
 
